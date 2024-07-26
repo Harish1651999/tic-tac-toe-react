1eb87b2e73bbe70e697e934370606b531764acc2
Harish1651999 <hk1651999@gmail.com>
2024-07-26 08:58:01 +0530

    add player component, conditional rendering and update state for edit.

---
+++ src/components/Player.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const Player = ({ name, symbol }) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(!isEditing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>;
+  let btnCaption = "Edit";
+
+  if (isEditing) {
+    playerName = <input type="text" value={name} required />;
+    btnCaption = "Save";
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+};
+
+export default Player;
