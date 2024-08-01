1ebcb89d0662602c1765ec9a1528dbba873063a0
Harish1651999 <hk1651999@gmail.com>
2024-08-01 08:13:20 +0530

    lift computed values up.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,21 +1,6 @@
 import React, { useState } from "react";
 
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-const GameBoard = ({ onSelectSquare, activePlayerSymbol, turns }) => {
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
+const GameBoard = ({ onSelectSquare, activePlayerSymbol, board }) => {
   // const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   // function handleSelectSquare(rowIndex, colIndex) {
@@ -32,7 +17,7 @@ const GameBoard = ({ onSelectSquare, activePlayerSymbol, turns }) => {
 
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
